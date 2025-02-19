22f97ff4a611fe53f3288f5d20409abd16d1b7eb
Uchiha@8 <2110107@newuu.uz>
2025-02-19 13:47:01 +0500

    Add no-args constructors to Bird and Dog classes

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -2,6 +2,10 @@ package com.epam.OOP;
 
 public class Bird extends Animal{
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
     public Bird(String color, int numberOfPaws, boolean hasFur) {
         super(color, numberOfPaws, hasFur);
     }
--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -2,6 +2,10 @@ package com.epam.OOP;
 
 public class Dog extends Animal {
 
+    public Dog() {
+        super("brown", 4, true);
+    }
+
     public Dog(String color, int numberOfPaws, boolean hasFur) {
         super(color, numberOfPaws, hasFur);
     }
