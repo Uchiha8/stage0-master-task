738b8d721f446eb9b12fd9e0b348f6c597c2f2bb
Uchiha@8 <2110107@newuu.uz>
2025-02-19 12:58:06 +0500

    Add no-args constructors to Dog and Bird classes
    - Added no-args constructor to Dog with default values.
    - Added no-args constructor to Bird with default values.
    - Overridden getDescription() method in Bird to include additional sentence.

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,14 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal{
 
+    public Bird(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+    }
+
+    @Override
+    public String getDescription() {
+        String result = super.getDescription();
+        return result + " Moreover, it has 2 wings and can fly.";
+    }
 }
